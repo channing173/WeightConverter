4d83611f83b25d3ea7d211723619b0b25aabed33
Channing Williams <cgwthree@gmail.com>
2026-02-07 17:05:39 -0500

    Auto-update PWA

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'weight-converter-v1';
+const CACHE_NAME = 'weight-converter-v2';
 const urlsToCache = [
   './index.html',
   './manifest.json'
@@ -6,6 +6,8 @@ const urlsToCache = [
 
 // Install service worker
 self.addEventListener('install', event => {
+  // Force the waiting service worker to become the active service worker
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
@@ -17,25 +19,31 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+        // Return cached version or fetch new version
+        return response || fetch(event.request).then(fetchResponse => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+          });
+        });
       })
   );
 });
 
 // Update service worker
 self.addEventListener('activate', event => {
+  // Take control of all pages immediately
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    clients.claim().then(() => {
+      return caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheName !== CACHE_NAME) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      });
     })
   );
 });
